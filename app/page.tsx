6db0b40bfe0d132ec593559d057ca688f34431ff
Leticia Gerhardt <leticiafloripasc@hotmail.com>
2023-09-08 14:39:26 -0300

    session cards

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Certificate } from "@/types/Certificate";
-import certificate from "@/sanity/schemas/certificate";
 
 export default async function Home() {
   const contents = await getContents();
@@ -22,23 +21,37 @@ export default async function Home() {
       <Header />
       <main>
         <section className="bg-emerald-800 p-4 text-white">
-          <p className="md:px-20 md:pb-20 md:pt-10 md:text-xl">
-            Aqui vai ter uma frase sobre a empresa
-          </p>
-          {contents.map((content: Certificate) => (
-            <Card key={content._id} className="bg-transparent text-white w-56 max-h-72">
-              <CardHeader>
-                <CardTitle>{content.name}</CardTitle>
-                <img src={content.image} className="w-12"></img>
-              </CardHeader>
-              <CardContent>
-                <CardDescription >{content.description}</CardDescription>
-              </CardContent>
-              <CardFooter>
-                <p className="text-green-500">Saiba mais</p>
-              </CardFooter>
-            </Card>
-          ))}
+          <div className="md:px-20 md:pb-32 md:pt-16">
+            <p className=" md:text-5xl font-semibold mb-3">
+              Lorem Ipsum is simply
+            </p>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s
+            </p>
+          </div>
+
+          <div className="md:px-20">
+            {contents.map((content: Certificate) => (
+              <Card
+                key={content._id}
+                className="bg-transparent text-white w-56 h-75 "
+              >
+                <CardHeader className="gap-3">
+                  <CardTitle>{content.name}</CardTitle>
+                  <img src={content.image} className="w-12"></img>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription>{content.description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                  <a className="text-green-500" href="">
+                    Saiba mais
+                  </a>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </section>
       </main>
     </div>
